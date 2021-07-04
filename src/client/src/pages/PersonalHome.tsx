99cd726f777c278c9be03a92b60b7c0e71839dd1
Brandon Kelley <brandonmkelley@outlook.com>
2021-07-04 10:51:55 -0700

    Tweaking content on the home page.

--- src/client/src/pages/PersonalHome.tsx
+++ src/client/src/pages/PersonalHome.tsx
@@ -21,12 +21,12 @@ export default () => {
         <React.Fragment>
             <div id="banner"></div>
 
-            <div className="container text-center pt-5 pb-5" style={{ fontSize: "1.4rem" }}>
-                "<i>We can only see a short distance ahead, but we can see plenty there that needs to be done.</i>"
+            <div className="container text-center pt-5 pb-5">
+                <span style={{ fontSize: "1.4rem" }}>"<i>We can only see a short distance ahead, but we can see plenty there that needs to be done.</i>"</span>
                 <br/><br/>
-                <span style={{ fontSize: '1.2rem' }}>Alan Turing in <i>Computing Machinery and Intelligence</i></span>
+                <span>Alan Turing in <i>Computing Machinery and Intelligence</i></span>
                 <br/>
-                <span style={{ fontSize: '1rem' }}><span>Turing, A. M. (1950). Computing Machinery and Intelligence. Mind, 59, 433–460. </span>
+                <span><span>Turing, A. M. (1950). Computing Machinery and Intelligence. Mind, 59, 433–460. </span>
                     <a target="_blank" href="https://doi.org/10.1093/mind/LIX.236.433">https://doi.org/10.1093/mind/LIX.236.433</a></span>
             </div>
 
@@ -34,10 +34,11 @@ export default () => {
                 <div className="container text-justify pt-5 pb-5">
                     Hello! Thank you for visiting my website. I am an engineer, friend, and problem solver in the solar industry.
                     I received my <b>B.S. in Computer Engineering</b> at California Polytechnic State University in
-                    San Luis Obispo, California, with culminating work in <b>advanced computer architecture</b> and <b>human-computer
-                    interaction</b>. I currently work as a Renewables Data Analyst in the Sustainable Solutions group
+                    San Luis Obispo, California, with culminating work in <b>human-computer
+                    interaction</b> and <b>advanced computer architecture</b>.
+                    I currently work as a Renewables Data Analyst in the Sustainable Solutions group
                     at the <a href="https://sustainablesolutions.duke-energy.com/" target="_blank">Duke Energy Corporation</a>.
-                    I collaborate with peers to address contemporary issues with research and the application of advanced
+                    I collaborate with peers to address contemporary issues in research and the application of advanced
                     technology. My favorite hobbies include reading, playing guitar, and changing my lifestyle.
                     Please feel free to reach out and connect with me.
                 </div>
@@ -54,10 +55,11 @@ export default () => {
                     <div className="col-12 col-md-6 col-lg-4 pt-md-5 pb-md-3">
                         <h4>Topics</h4>
                         <ul>
-                            <li>Computer Architecure</li>
-                            <li>Human-computer Interaction</li>
-                            <li>Embedded systems</li>
-                            <li>Cloud services</li>
+                            <li>Solar Industry Analytics</li>
+                            <li>Human-Computer Interaction</li>
+                            <li>Computer Architecture</li>
+                            <li>Embedded Systems</li>
+                            <li>Cloud Services</li>
                         </ul>
                         <h4>Work Experience</h4>
                         <ul>
@@ -69,10 +71,10 @@ export default () => {
                     <div className="col-12 col-md-6 col-lg-4 pt-md-5 pb-3">
                         <h4>Projects</h4>
                         <ul>
-                            <li>Solar site performance monitoring</li>
+                            <li>Solar site performance reporting</li>
                             <li>FPGA processor using RiSC-16 ISA</li>
                             <li>Pipelining and prediction architectures</li>
-                            <li>FMCW embedded radar processing</li>
+                            <li>Field-oriented pump control</li>
                             <li>Robotics &amp; augmented environments</li>
                             <li>LTE security in automotive applications</li>
                         </ul>
@@ -94,13 +96,16 @@ export default () => {
                     <div className="col-12 col-md-6 col-lg-4 pt-md-5 pb-md-3">
                         <h4>Team Involvement</h4>
                         <ul>
-                            <li>Cal Poly Computer Engineering Dept</li>
+                            <li>Trivia Night!</li>
+                            <li>Operational Excellence in Solar</li>
+                            <li>Grassroots Custom Satisfaction</li>
+                            <li>Cal Poly Computer Engineering</li>
                             <li>HackingEDU</li>
-                            <li>SLO Hacks</li>
                             <li>Sigma Phi Delta Engineering Fraternity</li>
                         </ul>
                         <h4>Events</h4>
                         <ul>
+                            <li>Farmers' markets</li>
                             <li>ASFP Out of the Darkness walk</li>
                             <li>Food bank volunteering</li>
                             <li>Grid Alternatives solar installation</li>
@@ -109,13 +114,13 @@ export default () => {
                     <div className="col-12 col-md-6 col-lg-4 pt-md-5 pb-3">
                         <h4>Locations</h4>
                         <ul>
-                            <li>Seattle, WA</li>
-                            <li>Whidbey Island, WA</li>
-                            <li>Bay Area, CA</li>
-                            <li>San Luis Obispo, CA</li>
                             <li>Orange County, CA (current)</li>
+                            <li>Everett, WA</li>
+                            <li>San Luis Obispo, CA</li>
+                            <li>Bay Area, CA</li>
+                            <li>Whidbey Island, WA</li>
                         </ul>
-                        <h4>Online</h4>
+                        <h4>Social</h4>
                         <div>
                             <a className="d-inline-block fa fa-facebook" href="https://facebook.com/14bmkelley" target="_blank"></a>
                             <a className="d-inline-block fa fa-twitter" href="https://twitter.com/14bmkelley" target="_blank"></a>
@@ -136,10 +141,10 @@ export default () => {
                         <h4>Readings</h4>
                         <ul>
                             <li><i>Factfulness</i>, Hans Rosling</li>
-                            <li><i>The Hitchhiker's Guide to the Galaxy</i>, Douglas Adams</li>
-                            <li><i>A Life in Parts</i>, Bryan Cranston</li>
+                            <li><i>The Hitchhiker's Guide to the Galaxy</i>,<br/>Douglas Adams</li>
+                            <li><i>I Know Why The Caged Bird Sings</i>,<br/>Maya Angelou</li>
                             <li><i>Anxious People</i>, Fredrik Backman</li>
-                            <li><i>Zen and the Art of Motorcycle Maintanence</i>, Robert Pirsig</li>
+                            <li><i>The Goal</i>, Eli Goldratt</li>
                         </ul>
                         <h4>Writings</h4>
                         <ul>
@@ -160,7 +165,7 @@ export default () => {
                             <li>Streaming and filmography</li>
                             <li>E-Sports and online gaming</li>
                             <li>Water and material conservation</li>
-                            <li>Responsible diet, unique meals</li>
+                            <li>Views as a veteran vegan</li>
                             <li>Personal productivity</li>
                         </ul>
                     </div>
