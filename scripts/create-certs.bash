63ea3048699fe06467e9001a860f57860d92b583
brandonmkelley <brandonmkelley@outlook.com>
2024-09-05 20:00:12 +0000

    Adding new build scripts

---
+++ scripts/create-certs.bash
@@ -0,0 +1,2 @@
+sudo certbot certonly --manual -d kelley-family.org -d 14bmkelley.com --agree-tos --manual-public-ip-logging-ok --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory --register-unsafely-without-email --rsa-key-size 4096
+
--- scripts/start.bash
+++ scripts/start.bash
@@ -5,9 +5,9 @@ sudo apt install immortal
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 4430
 
-sudo cp -L /etc/letsencrypt/live/14bmkelley.com/cert.pem ../src/server/static
-sudo cp -L /etc/letsencrypt/live/14bmkelley.com/fullchain.pem ../src/server/static
-sudo cp -L /etc/letsencrypt/live/14bmkelley.com/privkey.pem ../src/server/static
+sudo cp -L /etc/letsencrypt/live/kelley-family.org/cert.pem ../src/server/static
+sudo cp -L /etc/letsencrypt/live/kelley-family.org/fullchain.pem ../src/server/static
+sudo cp -L /etc/letsencrypt/live/kelley-family.org/privkey.pem ../src/server/static
 sudo chown ubuntu:ubuntu ../src/server/static/*
 chmod 600 ../src/server/static/*
 
